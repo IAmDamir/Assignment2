1fb8f33c49c6937945ce7b10b1bf7a01f85aaff2
Damir Temirgaliyev <temirgalievdamir03@gmail.com>
2021-09-27 16:26:29 +0600

    Made some part of 2-nd part

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main() {
     staticPerson = staticPerson.getLuck() > tempPerson.getLuck()? staticPerson : tempPerson;
   }
 
-  // outputting the person with highest luck
+  // outputting the person with the highest luck
   cout<< "Name: " << staticPerson.getName() << endl
       << "Age: " << staticPerson.getAge() << endl
       << "Luck: " << staticPerson.getLuck() << endl;
@@ -38,12 +38,39 @@ int main() {
    * then we need to use dynamic alloc.
    * But the execution of dynamic memory alloc. is slower than static memory alloc. */
   auto *dynamicPerson = new Person();
-   /* In the dynamic memory alloc. the memory is released manually.
-    * This means if we don't delete the dynamically allocated memory,
-    * it won't be released. Device cannot use the unreleased memory,
-    * it leaves less and less memory to the device and that's what we call memory leak.
-    * To prevent the memory leak we should release all dynamically allocated memory
-    * using delete function */
+  /* In the dynamic memory alloc. the memory is released manually.
+   * This means if we don't delete the dynamically allocated memory,
+   * it won't be released. Device cannot use the unreleased memory,
+   * it leaves less and less memory to the device and that's what we call memory leak.
+   * To prevent the memory leak we should release all dynamically allocated memory using delete function */
   delete dynamicPerson;
   return 0;
 }
+
+void cacheFriendly() {
+  /* Vector allocates objects one after another, and
+   * I think most methods of vector is iterating through neighbour elements,
+   * so I think that vector is an example of cache-friendly code */
+  vector<int> integers;
+  vector<vector<int>> twoDimensionalVector;
+  int integer = 0;
+
+  // Iterating through 2D array by rows is also considered cache-friendly
+  for (int i = 0; i < twoDimensionalVector.size(); ++i) {
+    for (int j = 0; j < twoDimensionalVector.at(i).size(); ++j) {
+      twoDimensionalVector[i][j] = integer;
+      integer++;
+    }
+  }
+}
+
+// We can use const keyword to show that we will not modify the pointer or reference passed in arguments.
+void constant(const string& theString, const string* theSecondString) {
+  // We can use const to make sure that some object will remain the same.
+  const int ten = 10;
+  // ten = 9; will force errors here.
+}
+// Also, we can use const at member functions.
+// In const member func.-s is not allowed to change value of attributes of its class.
+// But only const func.-s can be called by const object.
+// So if we want to call some func. with const obj. we forced to make this func. const.
